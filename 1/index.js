e61681f8ac48c7114d814c4132bb9c56856e2fcd
Bilol <bilolgafuroff@gmail.com>
2023-08-05 10:25:07 +0500

    function

---
+++ 1/index.js
@@ -0,0 +1,32 @@
+//! 1
+// function sumMul(n, m) {
+//     if (n >= m) return "INVALID";
+//     else {
+//         let result = 0;
+//         for (let i = n; i < m; i += i){
+//             result += i;
+//         };
+//         return result;
+//     };
+// };
+
+//! 2
+// const quarterOf = (month) => {
+//     switch(month) {
+//         case 1 : console.log("January"); break;
+//         case 2 : console.log("February"); break;
+//         case 3 : console.log("March"); break;
+//         case 4 : console.log("April"); break;
+//         case 5 : console.log("May"); break;
+//         case 6 : console.log("Juney"); break;
+//         case 7 : console.log("July"); break;
+//         case 8 : console.log("August"); break;
+//         case 9 : console.log("Septembr"); break;
+//         case 10 : console.log("Octembr"); break;
+//         case 11 : console.log("Novembr"); break;
+//         case 12 : console.log("Decembr"); break;
+//         default: console.log("Eroor 404");
+//     }
+//   }
+
+// quarterOf(18)
