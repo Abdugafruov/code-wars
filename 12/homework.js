a9013588b275eaa8be730c11abe637a0cb48de7e
Bilol <bilolgafuroff@gmail.com>
2023-08-20 15:10:58 +0500

    Calculate BMI

---
+++ 12/homework.js
@@ -0,0 +1,14 @@
+function bmi(weight, height) {
+    var formula = (weight / Math.pow(height, 2));
+    switch (true) {
+      case formula <=18.5:
+      return 'Underweight';
+      case formula <=25.0:
+      return 'Normal';
+      case formula <=30:
+      return 'Overweight';
+      default:
+      return 'Obese';
+      
+    }
+  }
